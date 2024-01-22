d3023747f329980bf7b97e0184e1d46fc026a0da
Nguyễn Ngọc Anh Tuấn <nnatuan2003@gmail.com>
2024-01-22 16:13:41 +0700

    ok

--- starter/server.js
+++ starter/server.js
@@ -1,7 +1,21 @@
+/* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: 'starter/config.env' });
+
+// connect to DB
+const DB = process.env.DATABASE.replace( // replace password with env variable
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+})
+.then(() => console.log('DB connection successful!'));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
