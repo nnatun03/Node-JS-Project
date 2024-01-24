728bcc5c3d5469b085839ab11460d0fb254877e4
Nguyễn Ngọc Anh Tuấn <nnatuan2003@gmail.com>
2024-01-24 15:00:39 +0700

    u

--- starter/models/tourModel.js
+++ starter/models/tourModel.js
@@ -7,9 +7,25 @@ const tourSchema = new mongoose.Schema({
       required: [true, 'A tour must have a name!'],
       unique: true,
     },
-    rating: {
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration!'],
+    },
+    MaxgroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size!'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty!'],
+    },
+    ratingAverage: {
       type: Number,
       default: 4.5,
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
     },
     price: {
       type: Number,
